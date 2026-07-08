9ccb3e7a9c8f0662dc27b0685acaa83853ab3d98
Vansh Saxena <vanshsaxena059@gmail.com>
2026-07-08 23:53:07 +0530

    Add environment variable support for API base URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
